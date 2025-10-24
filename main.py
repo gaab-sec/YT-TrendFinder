1e03011d3299ebaae640cde1d3f17b4185831c63
gaab-sec <gabrieljrg2006@gmail.com>
2025-10-23 21:29:03 -0300

    chore: centraliza constantes e melhora inicialização do script

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import datetime
 import sys
 
 import googleapiclient.discovery
+from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 from rich.console import Console
 from rich.table import Table
+
+
+# constantes
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+API_SERVICE_NAME = "youtube"
+API_VERSION = "v3"
+
 
 def main(query):
     print(f"Buscando vídeos para: '{query}'...")
@@ -35,11 +44,7 @@ def main(query):
 
 
 def get_videos_info(search_query, max_results=10, video_duration="any"):
-    # Loading enviroment variables
-    load_dotenv()
-    api_key = os.getenv('API_KEY')
 
-    # Getting datetime to 30 days ago
     utc_now = datetime.datetime.now(datetime.timezone.utc)
     utc_30days = datetime.timedelta(30)
 
@@ -50,12 +55,8 @@ def get_videos_info(search_query, max_results=10, video_duration="any"):
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
  
-    api_service_name = "youtube"
-    api_version = "v3"
-    DEVELOPER_KEY = api_key
- 
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey = DEVELOPER_KEY)
+        API_SERVICE_NAME, API_VERSION, developerKey = API_KEY)
  
     request = youtube.search().list(
         part="snippet",
@@ -81,7 +82,6 @@ def get_videos_info(search_query, max_results=10, video_duration="any"):
     for video in videos_info:
         videos_id.append(video["id"])
     
-    # extract videos statistics
     request = youtube.videos().list(
         part="statistics",
         id=videos_id
@@ -89,15 +89,16 @@ def get_videos_info(search_query, max_results=10, video_duration="any"):
     statistics = request.execute()["items"]
     view_count = [video_statistic["statistics"]["viewCount"] for video_statistic in statistics]
 
-    # add view count to videos info
     for index in range(len(view_count)):
         videos_info[index]["view_count"] = view_count[index]
 
     return videos_info
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        query = sys.argv[1]
-        main(query)
-    else:
-        raise "Erro: Você precisa fornecer um termo de busca."
+    if len(sys.argv) > 2:
+        print("ERRO: Você precisa fornecer um termo de busca.")
+        print("Ex: python main.py 'termo de busca'")
+        sys.exit(1)
+
+    query = "".join(sys.argv[1:])
+    main(query)
